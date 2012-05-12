0667fab4d6ad55a5e7ce067787930b53e24001fe
Kris Buytaert <sdog@inuits.be>
2012-05-12 09:55:15 +0200

    Epel package does not have correct status .. using pattern

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,13 +3,14 @@
 #
 class redis::service {
   service { 'redis-server':
-		ensure     => running,
-		enable     => true,
-		hasrestart => true,
-		hasstatus  => $operatingsystem ? {
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => $operatingsystem ? {
       /(?i)(Debian|Ubuntu)/ => false,
-      /(?i)(RedHat|CentOS)/ => true,
-		},
-		require    => Class['redis::config']
-	}
+      /(?i)(RedHat|CentOS)/ => false,
+    },
+    pattern    => 'redis-server',
+    require    => Class['redis::config']
+  }
 }
